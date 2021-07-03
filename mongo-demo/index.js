47e600aa87e0ab59912cfeeca5d2e32c23e4c73a
dungnc <dung.dev.gramer@gmail.com>
2021-07-04 03:31:47 +0700

    ➕ Save to MongoDB

---
+++ mongo-demo/index.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: "Angular Course",
+    author: "DungGramer",
+    tags: ["angular", "frontend"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+createCourse();
