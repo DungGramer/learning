1bd7593a1e9cfcac57944ae35558256404dec179
dungnc <dung.dev.gramer@gmail.com>
2021-12-21 18:11:46 +0700

    ✅ Done add todo

---
+++ learn-redux/todo-app-redux/src/redux/actions.js
@@ -0,0 +1,4 @@
+export const addTodo = (data) => ({
+  type: "todoList/addTodo",
+  payload: data,
+});
---
+++ learn-redux/todo-app-redux/src/redux/reducer.js
@@ -0,0 +1,31 @@
+const initState = {
+  filters: {
+    search: "",
+    status: "All",
+    priority: [],
+  },
+  todoList: [
+    { id: 1, name: "Learn Yoga", completed: false, priority: "Medium" },
+    { id: 2, name: "Learn Redux", completed: true, priority: "Hight" },
+    { id: 3, name: "Learn Javascript", completed: false, priority: "Low" },
+  ],
+};
+
+const rootReducer = (state = initState, action) => {
+  console.log(`📕 state, action - 15:reducer.js \n`, state, action);
+  switch (action.type) {
+    case "todoList/addTodo":
+      return {
+        ...state,
+        todoList: [
+          ...state.todoList,
+          action.payload,
+        ],
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
