c43ea4f74db36f964658f937e34aabc045a56d49
dungnc <dung.dev.gramer@gmail.com>
2022-03-31 20:11:51 +0700

    Add config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,34 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
   env: {
     browser: true,
     es2021: true,
     node: true,
     jest: true,
   },
-  extends: ['plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['prettier'],
+  plugins: ['@typescript-eslint'],
   rules: {
     semi: 0,
     'no-console': 'warn',
     'comma-dangle': 0,
     'prettier/prettier': 'error',
+
+    '@typescript-eslint/no-unused-vars': 'error',
+    // to enforce using type for object type definitions, can be type or interface
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
   },
 };
