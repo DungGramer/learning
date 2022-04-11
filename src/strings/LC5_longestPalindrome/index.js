b072079e8a2e7423ea68fc72e06b04232d1f0c2b
dungnc <dung.dev.gramer@gmail.com>
2022-04-11 10:37:25 +0700

    ✅ LC5_longestPalindrome

--- src/strings/LC5_longestPalindrome/index.js
+++ src/strings/LC5_longestPalindrome/index.js
@@ -1,3 +1,26 @@
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+  let startIndex = 0;
+  let maxLength = 1;
+
+  function expandAroundCenter(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      const currentPalLength = right - left + 1;
+      if (currentPalLength > maxLength) {
+        startIndex = left;
+        maxLength = currentPalLength;
+      }
+
+      left -= 1;
+      right += 1;
+    }
+  }
+
+  for (let i = 0; i < s.length; ++i) {
+    expandAroundCenter(i - 1, i + 1);
+    expandAroundCenter(i, i + 1);
+  }
+
+  return s.substring(startIndex, startIndex + maxLength);
+}
 
 module.exports = longestPalindrome;
