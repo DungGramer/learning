100bdaeae9ceb831a432bc6cba20c1700cb82783
dungnc <dungnc@coccoc.com>
2022-04-19 14:38:25 +0700

    ✅ ClimbStiars

--- src/dynamicProgramming/LC70_climbStairs/index.js
+++ src/dynamicProgramming/LC70_climbStairs/index.js
@@ -1,3 +1,19 @@
-function climbStairs(n) {}
+function climbStairs(n) {
+  if (n === 1) return 1;
+  if (n === 2) return 2;
+
+  const cache = Array(n + 1);
+  cache[0] = 0;
+  cache[1] = 1;
+  cache[2] = 2;
+
+  let i = 3;
+  while(i <= n) {
+    cache[i] = cache[i - 1] + cache[i - 2];
+    i++;
+  }
+
+  return cache[n];
+}
 
 module.exports = climbStairs;
