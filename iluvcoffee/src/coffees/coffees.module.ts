14e14115eb47167ead460f80435eb3dc0c5f46eb
dungnc <dungnc@coccoc.com>
2022-07-15 17:55:02 +0700

    Create typeORM

--- iluvcoffee/src/coffees/coffees.module.ts
+++ iluvcoffee/src/coffees/coffees.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from 'src/coffees/coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from 'src/coffees/entities/coffee.entity';
 
-@Module({ controllers: [CoffeesController], providers: [CoffeesService] })
+@Module({
+  imports: [TypeOrmModule.forFeature([Coffee])],
+  controllers: [CoffeesController],
+  providers: [CoffeesService],
+})
 export class CoffeesModule {}
