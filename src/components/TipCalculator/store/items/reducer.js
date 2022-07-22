2884812e128f45f45708006101eb4eda0e691256
dungnc <dungnc@coccoc.com>
2022-07-22 14:40:38 +0700

    Move reducer to immer

--- src/components/TipCalculator/store/items/reducer.js
+++ src/components/TipCalculator/store/items/reducer.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import {
   ITEM_ADDED,
   ITEM_PRICE_UPDATED,
@@ -12,11 +13,10 @@ export const initialItems = [
   { uuid: id++, name: 'Vegan Ham', price: 12, quantity: 1 },
 ];
 
-export const reducer = (state = initialItems, action) => {
+export const reducer = produce((state = initialItems, action) => {
   if (action.type === ITEM_ADDED) {
     const item = { uuid: id++, quantity: 1, ...action.payload };
-
-    return [...state, item];
+    state.push(item);
   }
 
   if (action.type === ITEM_REMOVED) {
@@ -24,24 +24,14 @@ export const reducer = (state = initialItems, action) => {
   }
 
   if (action.type === ITEM_PRICE_UPDATED) {
-    return state.map((item) => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, price: action.payload.price };
-      }
-      return item;
-    });
+    const item = state.find((item) => item.uuid === action.payload.uuid);
+    item.price = parseInt(action.payload.price, 10);
   }
 
   if (action.type === ITEM_QUANTITY_UPDATED) {
-    return state.map((item) => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, quantity: action.payload.quantity };
-      }
-      return item;
-    });
+    const item = state.find((item) => item.uuid === action.payload.uuid);
+    item.quantity = parseInt(action.payload.quantity, 10);
   }
-
-  return state;
-};
+}, initialItems);
 
 export default reducer;
