e66a20d1690f5c62445360b43ccfa88768a93e81
DungGramer <dung.dev.gramer@gmail.com>
2022-12-12 00:38:17 +0700

    15. Share and Reuse functionality with libraries in Nx

--- libs/store/feature-game-detail/src/lib/store-feature-game-detail.tsx
+++ libs/store/feature-game-detail/src/lib/store-feature-game-detail.tsx
@@ -5,25 +5,79 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { CardMedia } from '@material-ui/core';
+import { formatRating } from '@nxegghead/store/util-formatters';
 
 /* eslint-disable-next-line */
-export interface StoreFeatureGameDetailProps {
-}
+export interface StoreFeatureGameDetailProps {}
 
 export function StoreFeatureGameDetail() {
   const params = useParams();
 
+  const [state, setState] = useState<{
+    data: any;
+    loadingState: 'success' | 'error' | 'loading';
+  }>({
+    data: {},
+    loadingState: 'success',
+  });
+
+  useEffect(() => {
+    setState({
+      ...state,
+      loadingState: 'loading',
+    });
+    const gameId = params.id;
+    fetch(`/api/games/${gameId}`)
+      .then((x) => x.json())
+      .then((res) => {
+        setState({
+          ...state,
+          data: res,
+          loadingState: 'success',
+        });
+      })
+      .catch((err) => {
+        setState({
+          ...state,
+          loadingState: 'error',
+        });
+      });
+  }, [params.id]);
+
   return (
     <div className="container">
-      <Card>
-        <CardActionArea>
-          <CardContent>
-            <Typography variant="body2" color="textSecondary" component="p">
-              {params.id}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+      {state.loadingState === 'loading' ? (
+        'Loading...'
+      ) : state.loadingState === 'error' ? (
+        <div>Error fetching data</div>
+      ) : state.data == null ? (
+        ''
+      ) : (
+        <Card>
+          <CardActionArea>
+            <CardMedia
+              className="game-card-media"
+              image={state.data.image}
+              title={state.data.name}
+            />
+            <CardContent>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {state.data.name}
+              </Typography>
+              <Typography
+                variant="body2"
+                color="textSecondary"
+                component="p"
+                className="game-rating"
+              >
+                <strong>Rating:</strong> {formatRating(state.data.rating)}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      )}
     </div>
   );
 }
