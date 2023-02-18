ea62a4644652b9e3e09833c5a2eb725373b1c7bb
DungGramer <dung.dev.gramer@gmail.com>
2023-02-18 18:29:35 +0700

    add toggle pan

--- main.js
+++ main.js
@@ -4,19 +4,57 @@ const initCanvas = (id) => {
   return new fabric.Canvas(id, {
     width: 500,
     height: 500,
+    selection: false,
   });
-}
+};
 
 const setBackground = (url, canvas) => {
   fabric.Image.fromURL(url, (img) => {
     canvas.backgroundImage = img;
     canvas.renderAll();
   });
+};
+
+function togglePan() {
+  currentMode = currentMode === modes.pan ? '' : modes.pan;
 }
 
-const canvas = initCanvas('canvas');
-setBackground('https://picsum.photos/200/300', canvas);
+const setPanEvents = (canvas) => {
+  canvas.on('mouse:move', (event) => {
+    if (mousePressed && currentMode === modes.pan) {
+      canvas.setCursor('grab');
+      const mEvent = event.e;
+      const delta = new fabric.Point(mEvent.movementX, mEvent.movementY);
+      canvas.relativePan(delta);
+    }
+  });
 
-canvas.on('mouse:move', (e) => {
-  console.log(e);
-});
+  canvas.on('mouse:down', (event) => {
+    mousePressed = true;
+
+    if (currentMode === modes.pan) {
+      canvas.setCursor('grab');
+    }
+  });
+
+  canvas.on('mouse:up', (event) => {
+    mousePressed = false;
+    canvas.setCursor('default');
+  });
+};
+
+const togglePanBtn = document.querySelector('.toggle-pan');
+togglePanBtn.addEventListener('click', togglePan);
+
+const toggleDrawBtn = document.querySelector('.toggle-draw');
+
+let currentMode;
+const modes = {
+  pan: 'pan',
+};
+const canvas = initCanvas('canvas');
+let mousePressed = false;
+
+setBackground('https://picsum.photos/300/300', canvas);
+
+setPanEvents(canvas);
