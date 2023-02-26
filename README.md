c837ce9c49339a9ac6d31629dac754c21f8eb436
DungGramer <dung.dev.gramer@gmail.com>
2023-02-26 11:18:45 +0700

    group, ungroup, add image

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fabric.js Tutorial Crash Course
+
+Video source: [Youtube](https://www.youtube.com/watch?v=mghXNWvVGTs&list=PLMz4kNt6Rs7gVDL18CmALxyykoDyhDgN0)
--- main.js
+++ main.js
@@ -73,11 +73,22 @@ const setPanEvents = (canvas) => {
   });
 };
 
-const clearCanvas = (canvas) => {
+const clearCanvas = (canvas, state) => {
+  state.val = canvas.toSVG();
   canvas.getObjects().forEach((obj) => {
     if (obj === canvas.backgroundImage) return;
     canvas.remove(obj);
   });
+};
+
+const restoreCanvas = (canvas, svgState, bgUrl) => {
+  if (svgState.val) {
+    fabric.loadSVGFromString(svgState.val, (objects) => {
+      objects = objects.filter((o) => o['xlink:href'] !== bgUrl);
+      canvas.add(...objects);
+      canvas.requestRenderAll();
+    });
+  }
 };
 
 const createRect = (canvas) => {
@@ -129,10 +140,45 @@ const createCirc = (canvas) => {
   });
   circle.on('selected', () => {
     circle.set('fill', 'red');
+    canvas.requestRenderAll();
   });
   circle.on('deselected', () => {
     circle.set('fill', 'tomato');
+    canvas.requestRenderAll();
   });
+};
+const groupObjects = (canvas, group, shouldGroup) => {
+  if (shouldGroup) {
+    const objects = canvas.getObjects();
+    group.val = new fabric.Group(objects, { cornerColor: 'white' });
+    clearCanvas(canvas);
+    canvas.add(group.val);
+    canvas.requestRenderAll();
+  } else {
+    group.val.destroy();
+    const oldGroup = group.val.getObjects();
+    canvas.remove(group.val);
+    canvas.add(...oldGroup);
+    group.val = null;
+    canvas.requestRenderAll();
+  }
+};
+
+const imgAdded = (e) => {
+  const files = e.target.files;
+
+  for (let i = 0, f; (f = files[i]); i++) {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      fabric.Image.fromURL(event.target.result, (img) => {
+        const img1 = img.set({ left: 0, top: 0, angle: 0 });
+        canvas.add(img1);
+        canvas.requestRenderAll();
+      });
+    };
+
+    reader.readAsDataURL(f);
+  }
 };
 
 const togglePanBtn = document.querySelector('.toggle-pan');
@@ -153,17 +199,32 @@ colorPickerLabel.addEventListener('change', (event) => {
 });
 
 const clearCanvasBtn = document.querySelector('.clear-canvas');
-clearCanvasBtn.addEventListener('click', () => clearCanvas(canvas));
+clearCanvasBtn.addEventListener('click', () => clearCanvas(canvas, svgState));
+const restoreCanvasBtn = document.querySelector('.restore-canvas');
+restoreCanvasBtn.addEventListener('click', () =>
+  restoreCanvas(canvas, svgState, bgUrl)
+);
 
 const addRectBtn = document.querySelector('.add-rect');
 addRectBtn.addEventListener('click', () => createRect(canvas));
 const addCircBtn = document.querySelector('.add-circ');
 addCircBtn.addEventListener('click', () => createCirc(canvas));
 
+const groupBtn = document.querySelector('.group');
+groupBtn.addEventListener('click', () => groupObjects(canvas, group, true));
+const unGroupBtn = document.querySelector('.un-group');
+unGroupBtn.addEventListener('click', () => groupObjects(canvas, group, false));
+
+const imgInput = document.querySelector('#img-input');
+imgInput.addEventListener('change', imgAdded);
+
 const canvas = initCanvas('canvas');
 let mousePressed = false;
 let color = '#000000';
+let group = {};
+let svgState = {};
+const bgUrl = 'https://picsum.photos/300/300';
 
-setBackground('https://picsum.photos/300/300', canvas);
+setBackground(bgUrl, canvas);
 
 setPanEvents(canvas);
