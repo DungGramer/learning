0c290d36f76ad5cf1c61b6dbf1adf92f00cf98aa
DungGramer <dung.dev.gramer@gmail.com>
2023-04-20 00:22:20 +0700

    continue 05

--- my/04-webpack/src/script.js
+++ my/04-webpack/src/script.js
@@ -1,31 +1,61 @@
 import "./style.css";
 import * as THREE from "three";
 
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
 // Scene
 const scene = new THREE.Scene();
 
-// Object
+/**
+ * Objects
+ */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-// Size
+// position
+/* mesh.position.x = 0.7;
+mesh.position.y = -0.6;
+mesh.position.z = 1; */
+mesh.position.set(0.7, -0.6, 1);
+
+// Scale
+/* mesh.scale.x = 2;
+mesh.scale.y = 0.5;
+mesh.scale.z = 0.5; */
+mesh.scale.set(2, 0.5, 0.5);
+
+// Rotation
+mesh.rotation.reorder("YXZ");
+mesh.rotation.y = 1;
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(1);
+scene.add(axesHelper);
+
+/**
+ * Sizes
+ */
 const sizes = {
   width: 800,
   height: 600,
 };
 
-// Camera
+/**
+ * Camera
+ */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// camera.position.x = 2;
 camera.position.z = 3;
-
 scene.add(camera);
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector(".webgl"),
+  canvas: canvas,
 });
-
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
