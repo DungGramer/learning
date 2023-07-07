12b7be8f37e012061c29296e4e2fc0d9f77257b2
DungGramer <dung.dev.gramer@gmail.com>
2023-07-07 08:19:09 +0700

    I am Rich app

---
+++ Development/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Dũng Gramer on 07/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
