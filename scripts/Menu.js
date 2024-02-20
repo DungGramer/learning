def814548c3f5984e8d853a8245ff9766e45666b
DungGramer <dung.dev.gramer@gmail.com>
2024-02-20 09:48:49 +0700

    21. Caching image

--- scripts/Menu.js
+++ scripts/Menu.js
@@ -33,6 +33,15 @@ const Menu = {
       }
     }
     Menu.render();
+
+    if (Menu.data) {
+      const imageCache = await caches.open("cm-images");
+      Menu.data.forEach((category) =>
+        imageCache.addAll(
+          category.products.map((p) => `/data/images/${p.image}`)
+        )
+      );
+    }
   },
   loadCacheFirst: async () => {
     // Cache first
